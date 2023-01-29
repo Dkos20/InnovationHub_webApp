cb62fda475098028a7d80be7eb0812464a0eee00
Dorijan Kos <dorijan.kos@epitech.eu>
2023-01-29 22:08:23 +0100

    .

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run dev -- --port 8080
+web: npm run dev
--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  port: 8080,
+  port: process.env.PORT || 8080,
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
