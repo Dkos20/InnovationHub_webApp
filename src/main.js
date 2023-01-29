cad92b86c3ae1776e788170b41e91072badb1339
Dorijan Kos <dorijan.kos@epitech.eu>
2023-01-29 22:31:23 +0100

    .

--- src/main.js
+++ src/main.js
@@ -7,13 +7,9 @@ import '@formkit/icons';
 import './assets/main.css';
 import '@meforma/vue-toaster';
 
+
 const app = createApp(App).use(plugin, defaultConfig);
 
 app.use(router)
 
 app.mount('#app')
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
